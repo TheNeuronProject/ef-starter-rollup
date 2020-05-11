f5c6373558293d9c7433ee3bf1264b868e7ffc80
Yukino Song <yukino@yuki.network>
2020-05-11 23:46:38 +0800

    Upgrade dependencies

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import Hello from './hello.eft'
 // Import helper function and version info from `ef-core`
 import {version} from 'ef-core'
 
-// Create an instance for template `App`
+// Create an instance for template `Hello`
 const app = new Hello({
 	$data: {
 		year: (new Date()).getFullYear(),
